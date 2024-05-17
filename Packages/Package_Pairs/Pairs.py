a67ad876773cd688fa892fbb029f9e308f9ffceb
Federico Deniard <fededeniard@gmail.com>
2024-05-16 23:16:45 -0300

    Added a function to print the relationship as a matrix

--- Packages/Package_Pairs/Pairs.py
+++ Packages/Package_Pairs/Pairs.py
@@ -8,7 +8,12 @@ def get_set():
         number = input("Ingrese un valor para el cojunto\n(aprete enter para omitir)\n")
         if not validate_number(number):
             break
-        A.append(number)
+        if number not in A:
+            number = int(number)
+            A.append(number)
+        else:
+            print(f"{number} ya se encuentra en el conjunto")
+            system("pause")
         system("cls")
     
     return A
@@ -162,3 +167,52 @@ def relationship(A: list,B: list) -> list:
                             if a != b:
                                 C.append([a,b])
     return C
+
+def relationship_matrix(A: list, B:list, AxB: list, C: list) -> list:
+    """Makes a matrix to check values from 2 sets
+
+    Args:
+        A (list): First set. It's used to build the matrix's rows
+        B (list): Second set. It's used to build the matrix's colums
+        AxB (list): Product between A and B
+        C (list): Relationship between A and B
+
+    Returns:
+        list: A matrix with A values as rows and B values as colums. Matrix[i][j] has 0 or 1 value if the relationship is false or true.
+    """
+    D = [[0] * len(B) for _ in range(len(A))]
+    row = 0
+    column = 0
+    j = 0
+
+    loop = True
+    while loop:
+        for i in range(len(AxB)):
+            if column == len(B):
+                column = 0
+                row += 1
+            if AxB[i] == C[j]:
+                D[row][column] = 1
+                j += 1
+            column +=1
+            if j == len(C) or i == len(AxB):
+                loop = False
+                break
+    return D
+
+def show_array(array: list):
+    """ Recieves an array and prints a formatted version of it.
+
+    Args:
+        array (list): The array you want to print.
+
+    """
+    message = "[ "
+    for i in range(len(array)):
+        for j in range(len(array[i])):
+            message += f"{array[i][j]} "
+        if i == len(array) - 1:
+            message += "]"
+        else:
+            message += "\n  "
+    return message
--- main.py
+++ main.py
@@ -14,32 +14,51 @@ def main():
         option = get_int(message="Seleccione una opcion\n1. AxB\n2. BxA\n3. AxA\n4. BxB\n5. Cambiar valores\n6. Salir\n",min=1,max=6)
         match option:
             case 1:
-                result = multiply_sets(A,B)
-                print(result)
-                relation = relationship(A,B)
-                print(relation)
+                result = multiply_sets(A, B)
+                print(f"AxB: {result}")
+                relation = relationship(A, B)
+                print(f"AxB: {result}")
+                print(f"R: {relation}")
+                matrix = relationship_matrix(A, B, result, relation)
+                formatted_matrix = show_array(matrix)
+                print(f"Mr: \n{formatted_matrix}")
+
             case 2:
-                result = multiply_sets(B,A)
-                print(result)
-                relation = relationship(B,A)
-                print(relation)
-                
+                result = multiply_sets(B, A)
+                print(f"BxA: {result}")
+                relation = relationship(B, A)
+                print(f"BxA: {result}")
+                print(f"R: {relation}")
+                matrix = relationship_matrix(B, A, result, relation)
+                formatted_matrix = show_array(matrix)
+                print(f"Mr: \n{formatted_matrix}")
+
             case 3:
-                result = multiply_sets(A,A)
-                print(result)
-                relation = relationship(A,A)
-                print(relation)
+                result = multiply_sets(A, A)
+                print(f"AxA: {result}")
+                relation = relationship(A, A)
+                print(f"AxA: {result}")
+                print(f"R: {relation}")
+                matrix = relationship_matrix(A, A, result, relation)
+                formatted_matrix = show_array(matrix)
+                print(f"Mr: \n{formatted_matrix}")
+
             case 4:
-                result = multiply_sets(B,B)
-                print(result)
-                relation = relationship(B,B)
-                print(relation)
+                result = multiply_sets(B, B)
+                print(f"BxB: {result}")
+                relation = relationship(B, B)
+                print(f"BxB: {result}")
+                print(f"R: {relation}")
+                matrix = relationship_matrix(B, B, result, relation)
+                formatted_matrix = show_array(matrix)
+                print(f"Mr: \n{formatted_matrix}")
+
             case 5:
                 print("Conjunto A:")
                 A = get_set()
                 print("Conjunto B:")
                 B = get_set()
+
             case 6:
                 run = False
         system("pause")
-main()
